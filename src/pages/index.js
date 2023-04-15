a352fefbe62d1453f16b384a136f46072270f4fc
Harvard-DCE-CSCIE3 <lbouthillier@fas.harvard.edu>
2023-04-15 14:24:25 -0700

    layout and received pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,46 @@
 import * as React from "react"
 import { StaticImage } from 'gatsby-plugin-image'
+import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
-    <div>
-    <h3>Cloud Atlas Stuff</h3>
-    <p>Building on the Atlas metaphor, exploring Fetlify Forms, and Serverless Functions</p>
-    <StaticImage  src="../images/cloudatlas-poster.jpg" 
-      height={600} 
-      alt="Cloud Atlas Poster"
-      placeholder="blurred"
-      loading="eager"
-      >
-      </StaticImage>
-      <form method="post" name="Data Collection Form" data-netlify="true">
-        <input type="hidden" name="form-name" value="Data Collection Form"/>
-        <label>
-          Name
-          <input type="text" name="name" id="name" />
-        </label>
-        <label>
-          Email
-          <input type="email" name="email" id="email" />
-        </label>
-        <label>
-          Subject
-          <input type="text" name="subject" id="subject" />
-        </label>
-        <label>
-          Message
-          <textarea name="message" id="message" rows="5" />
-        </label>
-        <button type="submit">Send</button>
-        <input type="reset" value="Clear" />
-      </form>
-  </div>
+    <Layout>
+      <div>
+        <h3>Cloud Atlas Stuff</h3>
+        <p>Building on the Atlas metaphor, exploring Fetlify Forms, and Serverless Functions</p>
+        <StaticImage  src="../images/cloudatlas-poster.jpg" 
+          height={600} 
+          alt="Cloud Atlas Poster"
+          placeholder="blurred"
+          loading="eager"
+          >
+          </StaticImage>
+          <form method="post" 
+                name="Data Collection Form" 
+                data-netlify="true"
+                action='/messagereceived'>
+            <input type="hidden" name="form-name" value="Data Collection Form"/>
+            <label>
+              Name
+              <input type="text" name="name" id="name" />
+            </label>
+            <label>
+              Email
+              <input type="email" name="email" id="email" />
+            </label>
+            <label>
+              Subject
+              <input type="text" name="subject" id="subject" />
+            </label>
+            <label>
+              Message
+              <textarea name="message" id="message" rows="5" />
+            </label>
+            <button type="submit">Send</button>
+            <input type="reset" value="Clear" />
+          </form>
+      </div>
+    </Layout>
   )
 }
 
---
+++ src/pages/messagereceived.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const MessageReceivedPage = () => (
+  <Layout>
+    <h1>Contact</h1>
+    <p>Thank you for your submission!</p>
+  </Layout>
+);
+
+export default MessageReceivedPage;
+export const Head = () => <title>Message Page</title>
