e907419e6ec666d20c15355d3a6d8d0b0e5ddf64
Harvard-DCE-CSCIE3 <lbouthillier@fas.harvard.edu>
2023-04-17 19:34:34 -0700

    added confirm page

---
+++ src/pages/index-action.js
@@ -0,0 +1,41 @@
+import * as React from "react"
+import { StaticImage } from 'gatsby-plugin-image'
+import Layout from '../components/layout'
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <div>
+        <h3>Cloud Atlas Stuff</h3>
+        <p>Building on the Atlas metaphor, exploring Netlify Forms with an <code>action</code> attribute</p>
+        <StaticImage  src="../images/cloudatlas-poster.jpg" 
+          height={600} 
+          alt="Cloud Atlas Poster"
+          placeholder="blurred"
+          loading="eager"
+          >
+          </StaticImage>
+          <form method="post" 
+                name="Data Collection Form" 
+                data-netlify="true"
+                action="confirm">
+            <input type="hidden" name="form-name" value="Data Collection Form"/>
+            <label>
+              Name
+              <input type="text" name="name" id="name" />
+            </label><br/>
+            <label>
+              Email
+              <input type="email" name="email" id="email" />
+            </label><br/>
+            <button type="submit">Send</button>
+            <input type="reset" value="Clear" />
+          </form>
+      </div>
+    </Layout>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
