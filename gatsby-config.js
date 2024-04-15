86dde4a5bddfb18440756eddd18a1456affe8add
Larry Bouthillier <lbouthillier@post.harvard.edu>
2024-04-15 15:58:51 -0700

    updated for 2024

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,22 +3,8 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `gatsby-netlify-forms`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Gatsby Forms and Serverless Functions 2024`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
-};
+  plugins: [],
+}
